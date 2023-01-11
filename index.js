983ddcd549a84dc40839dc89d006907f69c189ba
Marco Carrozzo <ozeta@users.noreply.github.com>
2023-01-11 00:00:11 +0000

    reading file

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const core = require('@actions/core');
 const wait = require('./wait');
+const yaml = require('js-yaml');
+const fs   = require('fs');
 
+async function read_yml() {
 
-async function run() {
-  // noop
+  
+  // Get document, or throw exception on error
+  try {
+    const doc = yaml.load(fs.readFileSync('/inputs/base.yml', 'utf8'));
+    console.info("~~~READING YML FILE~~~~")
+    console.log(doc);
+    console.log(doc.env.VAR_A);
+    core.exportVariable("VAR_A", VAR_A);
+  } catch (e) {
+    console.log(e);
+  }
+
 }
 
 // most @actions toolkit packages have async methods
@@ -17,6 +30,7 @@ async function run() {
     core.info((new Date()).toTimeString());
 
     core.setOutput('time', new Date().toTimeString());
+    read_yml()
   } catch (error) {
     core.setFailed(error.message);
   }
