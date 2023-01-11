131224b153d61095a3a3e50394304c1be0f1edb3
Marco Carrozzo <Marco.Carrozzo@hyland.com>
2023-01-11 15:47:42 +0100

    wip

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
 const core = require('@actions/core');
-const wait = require('./wait');
 const yaml = require('js-yaml');
 const fs   = require('fs');
 
-async function read_yml() {
-
-  
-  // Get document, or throw exception on error
-  try {
-    const doc = yaml.load(fs.readFileSync('inputs/base.yml', 'utf8'));
-    core.info("~~~READING YML FILE~~~~")
-    core.info(doc);
-    core.info(doc.env.VAR_A);
-    core.exportVariable("VAR_A", doc.env.VAR_A);
-  } catch (e) {
-    console.log(e);
-  }
-
-}
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const doc = yaml.load(fs.readFileSync('.github/workflows/test.yml', 'utf8'));
+    core.info("~~~READING YML FILE~~~~")
+    core.info("reading yml file: " + doc.env.VAR_A)
+    core.exportVariable("VAR_A", doc.env.VAR_A);
+    core.exportVariable("VAR_SECR_A", doc.env.SECRET_VAR);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    read_yml()
-    core.setOutput('time', new Date().toTimeString());
+    const doc1 = yaml.load(fs.readFileSync('.github/workflows/codeql-analysis.yml', 'utf8'));
+    core.info("~~~READING YML FILE~~~~")
+    core.info("reading yml file: " + doc1.env.VAR_D)
+    core.exportVariable("VAR_D", doc1.env.VAR_D);
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
